f63a49f50d9e5b925d3a2b392d765a313ccdfd66
Jesus Eduardo Garcia <73787071+Jgarcia18201@users.noreply.github.com>
2023-08-08 20:12:55 -0600

    Exercise 3

---
+++ Challenge3/MultipleLines.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This task requires multiple lines.");
+Console.WriteLine("So, here are the multiple lines.");
+Console.WriteLine("They don't have to say anything specific.");
+Console.WriteLine("They just have to be multiple lines.");
+Console.WriteLine("So, here are the multiple lines.");
+//There can be as many lines as you want in a program.
