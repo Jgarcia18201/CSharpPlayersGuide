900696f5fc6ab3b640269d6bc4d7dd1fecb050e3
Jesus Eduardo Garcia <73787071+Jgarcia18201@users.noreply.github.com>
2023-08-09 23:08:56 -0600

    Challenge6

---
+++ Challenge6/VariablesOne.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = 10;
+byte aSingleByte = 23;
+short aNumber = 2;
+long aBigNumber = 100;
+sbyte aTinyNumber = -20;
+ushort anotherNumber = 3;
+uint aWholeNumber = 4;
+ulong aBigInt = 10_000_000;
+char aChar = 'b';
+string aString = "Hello, Bucko.";
+float aFloat = 1.56f;
+double aDouble = 1.54;
+decimal aDecimal = 1.57m;
+bool itWorked = false;
+Console.WriteLine(num + " " + aSingleByte + " " + aNumber + " " + aBigNumber + " " + aTinyNumber + " " + anotherNumber + " " + 
+aWholeNumber + " " + aBigInt + " " + aChar + " " + aString + " " + aFloat + " " + aDouble + " " + aDecimal + " " + itWorked);
